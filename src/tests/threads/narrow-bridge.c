295767c477ce7a0af9f5e27cceb81d4fd8353853
Sh1Fu <shifu.work@gmail.com>
2022-11-23 22:24:07 +0000

    Add tmp bad solve. Problems with dir change

--- src/tests/threads/narrow-bridge.c
+++ src/tests/threads/narrow-bridge.c
@@ -5,9 +5,11 @@
 #include "threads/synch.h"
 #include "narrow-bridge.h"
 
-int all_cars[2][2];						 // An array of all machine types and numbers
-int current_dir;						 // Variable responsible for the current movement direction
-bool is_moving_started;					 // A simple Boolean variable to determine the first direction
+int all_cars[2][2]; // An array of all machine types and numbers
+int emer_count[2];
+int current_dir;		// Variable responsible for the current movement direction
+bool is_moving_started; // A simple Boolean variable to determine the first direction
+bool solo_emergency;
 struct semaphore bridge;				 // Semaphore to control bridge car's count
 struct semaphore emer_sema, normal_sema; // Semaphores responsible for the number of machines of a certain type that we can let in (queue of machines of a certain type
 struct lock data_lock;					 // A lock that blocks access to a critical section for data integrity
@@ -16,10 +18,12 @@ int void_emer, void_places;				 // emer_sema and normal_sema value fields kinda
 void narrow_bridge_init(void)
 {
 	memset(all_cars, 0, sizeof(all_cars));
-	current_dir = 0;
-	is_moving_started = false;
+	memset(emer_count, 0, sizeof(emer_count));
 	void_places = 2;
+	current_dir = 0;
 	void_emer = 0;
+	is_moving_started = false;
+	solo_emergency = false;
 	sema_init(&bridge, 2);
 	sema_init(&emer_sema, 0);
 	sema_init(&normal_sema, 0);
@@ -33,15 +37,31 @@ void arrive_bridge(enum car_priority prio, enum car_direction dir)
 	struct semaphore *current_car = NULL; // Temporary semaphore
 	if (is_moving_started == false)		  // Starting direction identification
 	{
-		current_dir = ((all_cars[1][0] >= all_cars[1][1]) ? dir_left : dir_right);
+		if (all_cars[car_emergency][dir_left] > all_cars[car_emergency][dir_right]) { 
+			current_dir = dir_left;
+		}
+		else if (all_cars[car_emergency][dir_left] == all_cars[car_emergency][dir_right]) {
+			current_dir = ((all_cars[0][0] >= all_cars[0][1]) ? dir_left : dir_right);
+		}
+		else {
+			current_dir = dir_right;
+		}
+		emer_count[0] = all_cars[1][0];
+		emer_count[1] = all_cars[1][1];
 		is_moving_started = true;
 	}
+
 	while (all_cars[1][0] + all_cars[1][1] != 0) // Find the emergency cars amongst all the cars to let them go ahead of you
 	{
 		if (prio == car_emergency && current_dir == dir) // If it is an emergency with the right direction of travel - send it to the bridge
 		{
 			if (--all_cars[prio][dir] == 0) // Change direction if there are no ambulances left in the right direction(Last ambulance!!)
-				current_dir ^= 1;
+			{ 
+				if (emer_count[dir] % 2 != 0)
+				{
+					solo_emergency = true;
+				}
+			}
 			current_car = &emer_sema; // Taking a queue where we will write an ambulance if there is not enough room on the bridge
 			break;
 		}
@@ -50,14 +70,11 @@ void arrive_bridge(enum car_priority prio, enum car_direction dir)
 			thread_yield(); // Looking for threads further
 		}
 	}
-	// printf("Current dir and counts: %d %d %d\n\n", current_dir, all_cars[1][0], all_cars[1][1]);
-	while (prio == car_normal)
+	while (prio == car_normal || (solo_emergency && current_car == NULL))
 	{
-		if (current_dir == dir || all_cars[prio][dir ^ 1] == 0)
+		if (current_dir == dir)
 		{
 			current_car = &normal_sema;
-			if (--all_cars[prio][dir] == 0)
-				current_dir ^= 1;
 			break;
 		}
 		else
@@ -86,16 +103,26 @@ void arrive_bridge(enum car_priority prio, enum car_direction dir)
 		OR
 		There are no more ambulances to send to the bridge
 		*/
-		if (((current_dir == dir || (current_dir == dir ^ 1 && prio == car_emergency && all_cars[1][0] + all_cars[1][1] != 0) || void_places == 2) && (prio == car_emergency || void_emer == 0)))
+		if (((current_dir == dir || void_places == 2) && (prio == car_emergency || (prio == car_normal && solo_emergency && all_cars[1][dir] == 0 && void_places == 1 && current_dir == dir) || void_emer == 0)))
 		{
 			sema_down(&bridge); // Sending the car to the bridge
 			void_places--;
 			sema_down(current_car);
+			if (prio == car_normal && solo_emergency && all_cars[1][dir] == 0 && void_places == 1 && current_dir == dir)
+			{
+				//printf("\n Change \n");
+				solo_emergency = false;
+			}
+			if (void_places == 0 && ((prio == car_emergency && all_cars[1][dir^1]) || (prio == car_normal && solo_emergency) || (all_cars[1][dir] + all_cars[0][dir] == 0 && all_cars[1][dir^1] + all_cars[0][dir^1] != 0)))
+			{
+				current_dir ^= 1;
+			}
 			if (current_car == &emer_sema)
 			{
 				void_emer--;
 			}
 			lock_release(&data_lock);
+			//printf("%d %d %d\n\n", prio, void_places, void_emer);
 			break;
 		}
 		lock_release(&data_lock);
