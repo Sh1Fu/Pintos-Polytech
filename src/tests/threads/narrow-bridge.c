d7c6305e25a1872aed854289398af35a3c671d98
Sh1Fu <shifu.work@gmail.com>
2022-11-24 19:08:43 +0000

    fix one problem and add comments

--- src/tests/threads/narrow-bridge.c
+++ src/tests/threads/narrow-bridge.c
@@ -7,27 +7,30 @@
 #include "threads/synch.h"
 #include "narrow-bridge.h"
 
-struct semaphore directions_sema[2][2];
-struct semaphore bridge_sema; // sema for occupied_places
-uint16_t cars_counts[2][2];
-uint16_t occupied_places = 0; // how much cars on occupied_places
-uint16_t current_cars_count, all_active_cars;
-uint16_t current_dir; // which dir goes next
-uint16_t type;
+struct semaphore directions_sema[2][2];		  // Matrix with all semaphores
+struct semaphore bridge_sema;				  // Semaphore of bridge
+uint16_t cars_counts[2][2];					  // Number of all types of cars
+uint16_t occupied_places = 0;				  // Occupied places on bridge
+uint16_t current_cars_count, all_active_cars; // Number of current all cars and non blocked cars
+uint16_t current_dir;						  // Current bridge direction
+uint16_t type;								  // Type of car
 bool is_moving_started;
 
 void narrow_bridge_init(void)
 {
 	is_moving_started = false;
 	type = 0;
-	sema_init(&bridge_sema, 1); // initialize sema of occupied_places
+	sema_init(&bridge_sema, 1);
 	for (int i = 0; i < 2; i++)
 	{
-		sema_init(&directions_sema[i][0], 0); // initialize semas of cars
+		sema_init(&directions_sema[i][0], 0);
 		sema_init(&directions_sema[i][1], 0);
 	}
 }
 
+/*
+Set two cars of one type if bridge if full emty
+*/
 void _up_two_to_bridge()
 {
 	for (uint8_t i = 0; i < 2; i++)
@@ -39,15 +42,18 @@ void _up_two_to_bridge()
 	type = 0;
 }
 
-void _up_solo_to_bridge() 
+void _up_solo_to_bridge()
 {
 	sema_down(&bridge_sema);
-	sema_up(&directions_sema[1][current_dir]);
+	sema_up(&directions_sema[type][current_dir]);
 	sema_down(&bridge_sema);
 	occupied_places++;
 	type = 0;
 }
 
+/*
+Move normal car with emergency if emergency is last on current direction
+*/
 void _last_emer_w_normal()
 {
 	sema_up(&directions_sema[1][current_dir]);
@@ -57,14 +63,17 @@ void _last_emer_w_normal()
 	occupied_places += 2;
 }
 
+/*
+Choose correct cars to move
+*/
 void move_to_bridge()
 {
-	if (cars_counts[1][current_dir] >= 2)
+	if (cars_counts[1][current_dir] >= 2) // We have two blocked emergency
 	{
 		type = 1;
 		_up_two_to_bridge();
 	}
-	else if (cars_counts[1][current_dir] == 1)
+	else if (cars_counts[1][current_dir] == 1) // Last emergency on current direction
 	{
 		if (cars_counts[0][current_dir] >= 1)
 		{
@@ -76,11 +85,11 @@ void move_to_bridge()
 			_up_solo_to_bridge();
 		}
 	}
-	else if (cars_counts[0][current_dir] >= 2)
+	else if (cars_counts[0][current_dir] >= 2) // We have two normal blocked cars and empty bridge
 	{
 		_up_two_to_bridge();
 	}
-	else if (cars_counts[0][current_dir] == 1)
+	else if (cars_counts[0][current_dir] == 1) // We have the last car on current direction
 	{
 		_up_solo_to_bridge();
 	}
@@ -96,21 +105,22 @@ void move_to_bridge()
 void arrive_bridge(enum car_priority prio, enum car_direction dir)
 {
 	cars_counts[prio][dir]++;
-	thread_yield();
+	thread_yield(); // Count all types of cars
 	if (!is_moving_started)
 	{
-		current_dir = ((cars_counts[1][0] > cars_counts[1][1] && cars_counts[1][0] != cars_counts[1][1]) ? dir_left : dir_right); // choose dir
-		if (cars_counts[1][0] == cars_counts[1][1]) current_dir = ((cars_counts[0][0] >= cars_counts[0][1]) ? dir_left : dir_right);
+		current_dir = ((cars_counts[1][0] > cars_counts[1][1] && cars_counts[1][0] != cars_counts[1][1]) ? dir_left : dir_right); // Choose start direction
+		if (cars_counts[1][0] == cars_counts[1][1])
+			current_dir = ((cars_counts[0][0] >= cars_counts[0][1]) ? dir_left : dir_right);
 		all_active_cars = cars_counts[0][0] + cars_counts[0][1] + cars_counts[1][0] + cars_counts[1][1];
 		is_moving_started = true;
 	}
-	if (all_active_cars > 1)
+	if (all_active_cars > 1) // Block all cars and move them to their semaphores
 	{
 		all_active_cars--;
 		sema_down(&directions_sema[prio][dir]);
 	}
 
-	if (all_active_cars == 1)
+	if (all_active_cars == 1) // If last non blocked car then we should start "list"
 	{
 		all_active_cars--;
 		for (uint8_t i = 0; i < 2; i++)
@@ -125,14 +135,16 @@ void arrive_bridge(enum car_priority prio, enum car_direction dir)
 void exit_bridge(enum car_priority prio, enum car_direction dir)
 {
 	cars_counts[prio][dir]--;
-	current_cars_count = cars_counts[0][0] + cars_counts[0][1] + cars_counts[1][0] + cars_counts[1][1];
+	current_cars_count = cars_counts[0][0] + cars_counts[0][1] + cars_counts[1][0] + cars_counts[1][1]; // Count all cars
 	occupied_places--;
-	if (current_cars_count && occupied_places % 2 == 0)
+	if (current_cars_count && occupied_places % 2 == 0) // If we have cars and our bridge is not empty
 	{
-		if (!cars_counts[1][current_dir ^ 1] && cars_counts[1][current_dir]) current_dir ^= 1;
-		if ((current_dir == dir_right || current_dir == dir_left) && !(cars_counts[0][current_dir ^ 1] + cars_counts[1][current_dir ^ 1])) current_dir ^= 1;
+		if (!cars_counts[1][current_dir ^ 1] && cars_counts[1][current_dir])
+			current_dir ^= 1;
+		if ((current_dir == dir_right || current_dir == dir_left) && !(cars_counts[0][current_dir ^ 1] + cars_counts[1][current_dir ^ 1]))
+			current_dir ^= 1;
 		current_dir ^= 1;
-		for (uint8_t i = 0; i < 2; i++)
+		for (uint8_t i = 0; i < 2; i++) // Move cars from bridge
 		{
 			sema_up(&bridge_sema);
 		}
